b078b87f46c6871b45df443cf545646b2b824fa9
Guillermo Villalta <mexcelus@gmail.com>
2022-02-20 11:51:48 -0700

    addingmiddleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,14 @@
 import { Express, Request, Response, NextFunction } from "express";
 import { createTaskHandler } from "./controllers/task.controller";
+import validateResource from "./middleware/validateResource";
+import { createTaskSchema } from "./schema/task.schema";
 
 const routes = (app: Express) => {
   app.get('/healthcheck', (req: Request, res: Response, next: NextFunction) => {
     res.sendStatus(200);
   });
 
-  app.post('/api/tasks', createTaskHandler);
+  app.post('/api/tasks', validateResource(createTaskSchema), createTaskHandler);
 }
 
 export default routes;
