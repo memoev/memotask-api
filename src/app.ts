a9abf58544d47c80c1222c2df4654c1dc19d57eb
Guillermo Villalta <mexcelus@gmail.com>
2022-02-21 22:26:12 -0700

    refactor to inject server into tests

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,12 @@
-import express from "express";
 import config from "config";
 import connect from "./utils/connect";
 import log from "./utils/logger";
-import routes from "./routes";
+import createServer from './utils/server';
 
 const port = config.get<number>('port')
-
-const app = express();
-
-app.use(express.json());
+const app  = createServer();
 
 app.listen(port, async () => {
     await connect();
     log.info(`Listening on port ${port}`);
-    routes(app);
 })
