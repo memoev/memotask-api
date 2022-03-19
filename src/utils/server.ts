daaa62ca0a3ca845243d5c08d0363528e13fcb1e
Memo Villalta <memo.villalta@academymortgage.com>
2022-03-18 23:03:21 -0600

    allowing cors

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import routes from "../routes";
 
 const createServer = () => {
   const app = express();
   app.use(express.json());
+  app.use(cors());
   routes(app);
   return app;
 }
